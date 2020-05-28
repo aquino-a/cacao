3cf88f2a0f48c1fa301c8c50f674c6c2e68f9939
alex <blackterrapin@gmail.com>
2020-05-28 12:23:10 +0900

    change angular config location to proper place

--- src/main/java/com/cacao/server/config/AngularRouteConfiguration.java
+++ src/main/java/com/cacao/server/config/AngularRouteConfiguration.java
@@ -1,4 +1,4 @@
-package com.cacao.server.controller;
+package com.cacao.server.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
