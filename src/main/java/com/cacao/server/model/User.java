5b318e0b652c639ec12ed891530c5ad9bfcb5c76
alex <blackterrapin@gmail.com>
2021-01-18 21:53:23 +0900

    fix user for authentication

--- src/main/java/com/cacao/server/model/User.java
+++ src/main/java/com/cacao/server/model/User.java
@@ -25,6 +25,8 @@ public class User extends AbstractAuthenticationToken {
     @Transient
     private Jwt token;
 
+    public User(){ super(null); }
+
     public User(Collection<GrantedAuthority> authorities) {
         super(authorities);
     }
