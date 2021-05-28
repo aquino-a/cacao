3ca917359e1e98da4a32516e54912fbc0255d7fc
Alex <29205589+aquino-a@users.noreply.github.com>
2021-05-28 14:33:30 +0900

    fix tomcat redirect
    Squashed commit of the following:
    
    commit 3d337bb1ec7bb66220bb4ebbe022f921bb6c0dd3
    Author: Alex <29205589+aquino-a@users.noreply.github.com>
    Date:   Fri May 28 13:52:34 2021 +0900
    
        add redirect connector
    
    commit ff1967368526b490872f78d744af768708e63109
    Author: Alex <29205589+aquino-a@users.noreply.github.com>
    Date:   Fri May 28 11:39:16 2021 +0900
    
        move redirect to mvc config

--- src/main/java/com/cacao/server/config/AngularRouteConfiguration.java
+++ src/main/java/com/cacao/server/config/AngularRouteConfiguration.java
@@ -1,5 +1,13 @@
 package com.cacao.server.config;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
@@ -16,6 +24,12 @@ import java.io.IOException;
 @Configuration
 public class AngularRouteConfiguration implements WebMvcConfigurer {
 
+    @Value("${server.port.http:8080}")
+        int httpPort;
+
+    @Value("${server.port:8443}")
+        int httpsPort;
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
 
@@ -31,5 +45,32 @@ public class AngularRouteConfiguration implements WebMvcConfigurer {
                 : new ClassPathResource("/static/index.html");
             }
         });
+    }
+
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+        var tomcat = new TomcatServletWebServerFactory() {
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint securityConstraint = new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+
+                var collection = new SecurityCollection();
+                collection.addPattern("/*");
+                securityConstraint.addCollection(collection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+        tomcat.addAdditionalTomcatConnectors(redirectConnector());
+        return tomcat;
+    }
+
+    private Connector redirectConnector() {
+        Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+        connector.setScheme("http");
+        connector.setPort(httpPort);
+        connector.setSecure(false);
+        connector.setRedirectPort(httpsPort);
+        return connector;
     }
 }
